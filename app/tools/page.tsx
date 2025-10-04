a053a116a520ef53ddb2160667fd2273083b4f6f
adityagupta0251 <itsokkadi0251@gmail.com>
2025-10-05 01:06:16 +0530

    added some files

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from 'react';
 import { Code, Cpu, Globe, Database, Layers, Terminal, ChevronDown, ChevronUp, TrendingUp, BarChart3 } from 'lucide-react';
 
